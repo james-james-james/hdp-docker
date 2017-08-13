f5ffd8535033423460a29d58d134c92949e4b824
Nimrod Milo <nimrodm@traxretail.com>
2017-08-13 10:04:10 +0300

    changes in the install script

--- install-cluster.sh
+++ install-cluster.sh
@@ -11,8 +11,10 @@ blueprint add --file /tmp/single-node-hdfs-yarn
 cluster build --blueprint single-node-hdfs-yarn
 cluster autoAssign
 cluster create --exitOnFinish true
+exit
 EOF
 
+clear
 clear
 
 lines=1
@@ -22,16 +24,18 @@ do
         lines=`echo tasks | /usr/jdk64/jdk1.8.0_112/bin/java -jar /tmp/ambari-shell.jar --ambari.host=sandbox | egrep '(PENDING)|(QUEUED)|(IN_PROGRESS)' | wc -l`
         echo $lines
 done
+
 amount_started=`echo 'services list' | /usr/jdk64/jdk1.8.0_112/bin/java -jar /tmp/ambari-shell.jar --ambari.host=sandbox | egrep -A 1000 'SERVICE\s+STATE' | tail -n +3 | head -n -2 | awk '{print $2}' | grep -v STARTED | wc -l`
 if [ $amount_started -eq 0 ]; then
         echo "done"
+
+        ambari-agent stop
+        sleep 10
+        ambari-server stop
+        sleep 10
+
         exit 0
 else
         echo "there was a problem :("
         exit 1
 fi
-
-ambari-agent stop
-sleep 10
-ambari-server stop
-sleep 10
