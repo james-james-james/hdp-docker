bb91fdc313d2fc862de79110b2589a39675ac6ec
Nimrod Milo <nimrodm@traxretail.com>
2017-08-14 10:28:29 +0300

    Made the output more readable

--- install-cluster.sh
+++ install-cluster.sh
@@ -18,19 +18,16 @@ cluster create --exitOnFinish true
 exit
 EOF
 
-clear
-clear
-
-
 # Wait until all tasks are completed
 lines=1
 while [ $lines -gt 0 ]
 do
         sleep 20
         lines=`echo tasks | /usr/jdk64/jdk1.8.0_112/bin/java -jar /tmp/ambari-shell.jar --ambari.host=sandbox | egrep '(PENDING)|(QUEUED)|(IN_PROGRESS)' | wc -l`
-        echo $lines
+        echo "Currently found $lines tasks running"
 done
 
+# verfiy that all the services started correcty
 amount_started=`echo 'services list' | /usr/jdk64/jdk1.8.0_112/bin/java -jar /tmp/ambari-shell.jar --ambari.host=sandbox | egrep -A 1000 'SERVICE\s+STATE' | tail -n +3 | head -n -2 | awk '{print $2}' | grep -v STARTED | wc -l`
 if [ $amount_started -eq 0 ]; then
         echo "done"
