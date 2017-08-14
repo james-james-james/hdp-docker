347a642d630130bb8fb7122ff86d0e126c6f31e3
Nimrod Milo <nimrodm@traxretail.com>
2017-08-14 22:24:03 +0300

    Better start script

--- start.sh
+++ start.sh
@@ -23,8 +23,4 @@ su -c "hdfs dfs -mkdir /user/Nimrod" hdfs
 su -c "hdfs dfs -chown -R Nimrod:Nimrod /user/Nimrod" hdfs
 su -c "hdfs dfs -put /tmp/single* /user/Nimrod/" hdfs
 
-while [ 10 -gt 0 ]
-do
-  sleep 60
-  echo "stuff"
-done
+bash
