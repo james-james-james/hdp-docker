85db2c5f547c9abbf6ec498ad8312a743503633d
Nimrod Milo <nimrodm@traxretail.com>
2017-08-14 10:22:40 +0300

    Added initiation to the script

--- start.sh
+++ start.sh
@@ -8,10 +8,19 @@ sleep 10
 # Start the ambari services
 ambari-server start
 ambari-agent start
-sleep 10
+sleep 20
 
 # send command to start all services
 echo 'services start' | /usr/jdk64/jdk1.8.0_112/bin/java -jar /tmp/ambari-shell.jar --ambari.host=sandbox
+sleep 20
+
+# reduce time by forcing the system to leave safemode
+su -c "hdfs dfsadmin -safemode leave" hdfs
 sleep 10
+echo 'services list' | /usr/jdk64/jdk1.8.0_112/bin/java -jar /tmp/ambari-shell.jar --ambari.host=sandbox
+
+su -c "hdfs dfs -mkdir /user/Nimrod" hdfs
+su -c "hdfs dfs -chown -R Nimrod:Nimrod /user/Nimrod" hdfs
+su -c "hdfs dfs -put /tmp/single* /user/Nimrod/" hdfs
 
 bash
