69def76156aef38f64a429adf9da38985a448ace
Nimrod Milo <nimrodm@traxretail.com>
2017-08-14 18:46:03 +0300

    Fixed build bugs because always ran in interactive mode

--- start.sh
+++ start.sh
@@ -23,4 +23,9 @@ su -c "hdfs dfs -mkdir /user/Nimrod" hdfs
 su -c "hdfs dfs -chown -R Nimrod:Nimrod /user/Nimrod" hdfs
 su -c "hdfs dfs -put /tmp/single* /user/Nimrod/" hdfs
 
+while [ 10 -gt 0 ]
+do
+  sleep 60
+done
+
 bash
